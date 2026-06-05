81b8d204dba25ee9d487db1c5a47983a4b6a42b3
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-04 21:37:24 -0400

    Refactor drivetrain initialization and update logic

--- src/main.ino
+++ src/main.ino
@@ -9,19 +9,13 @@ Use: The code that goes into ADD VI
 #include <PestoLink-Receive.h>
 #include <Alfredo_NoU3.h>
 #include <stdio.h>
-#include <MotorPID.h>
 
-// Drivetrain Init
-NoU_Motor frontLeftMotor(1);
-NoU_Motor frontRightMotor(4);
-NoU_Motor rearLeftMotor(8);
-NoU_Motor rearRightMotor(5);
+// In-File Dependencies
+#include "MotorPID.h"
+#include "Drivetrain.h"
 
 // Motor Functions Init
 // N/A; Nothing here for now
-
-// Drivetrain Init
-NoU_Drivetrain drivetrain(&frontLeftMotor, &frontRightMotor, &rearLeftMotor, &rearRightMotor);
 
 // PID Init
 // N/A; Nothing here for now
@@ -41,14 +35,7 @@ void setup() {
   NoU3.begin();
   NoU3.calibrateIMUs(); // this takes exactly one second. Do not move the robot during calibration.
 
-  // Allows for immediate robot movement from small movements
-  drivetrain.setMinimumOutput(0.1);
-
-  // Inversion Parts
-  frontLeftMotor.setInverted(true);
-  frontRightMotor.setInverted(true);
-  rearLeftMotor.setInverted(true);
-  rearRightMotor.setInverted(true);
+  beginDrivetrain();
 }
 
 void loop() {
@@ -66,27 +53,10 @@ void loop() {
   if (PestoLink.isConnected()) {
     // --- Robot Functions ---
 
-    // --- Drivetrain Code ---
-    // Sets Axes
-    float fieldPowerX = movementSpeed * PestoLink.getAxis(0);
-    float fieldPowerY = -movementSpeed * PestoLink.getAxis(1);
-    float rotationPower = -movementSpeed * PestoLink.getAxis(2);
-
-    // Get robot heading (in radians) from the gyro
-    float heading = NoU3.yaw * angular_scale;
-
-    // Rotate joystick vector to be robot-centric
-    float cosA = cos(heading);
-    float sinA = sin(heading);
-
-    float robotPowerX = fieldPowerX * cosA + fieldPowerY * sinA;
-    float robotPowerY = -fieldPowerX * sinA + fieldPowerY * cosA;
-
-    // set motor power
-    drivetrain.holonomicDrive(robotPowerX, robotPowerY, rotationPower);
+    updateDrivetrain(PestoLink.getAxis(0), PestoLink.getAxis(1), PestoLink.getAxis(2), movementSpeed);
     NoU3.setServiceLight(LIGHT_ENABLED);
   } else {
-    drivetrain.holonomicDrive(0, 0, 0); // lmao the example def took from us
+    updateDrivetrain(0, 0, 0, 0);
     NoU3.setServiceLight(LIGHT_DISABLED);
   }
 }
