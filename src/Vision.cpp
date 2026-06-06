e83622227989f45fa42506de28e2464b3e805907
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-06 12:22:37 -0400

    Update and rename vision.cpp to Vision.cpp

---
+++ src/Vision.cpp
@@ -0,0 +1,65 @@
+// Dependencies
+#include "Wire.h"
+#include "HUSKYLENS.h"
+
+HUSKYLENS huskylens;
+
+// Variables
+int xCenter = 0;
+int yCenter = 0;
+int width = 0;
+int height = 0;
+bool isTagVisable = false;
+
+// Lists
+int availableTags[3][5] = { // On the format of tagID, xCenter, yCenter, width, height
+  {0, 0, 0, 0, 0}, // Hub Tag
+  {0, 0, 0, 0, 0}, // Right Trench Tag
+  {0, 0, 0, 0, 0}, // Left Trench Tag
+};
+
+// Return Data Functions
+int* get_TagRow(int rowIndex) { return availableTags[rowIndex];}
+bool get_isTagVisable(){ return isTagVisable;}
+
+
+void beginVision(){
+  // Start I2C communication
+  Wire.begin(8,9); // SDA, SCL
+  // [NOTE]: These are the pins I used for I2C on a ESP32-S3 Dev Board.
+  // The pins may be different on the NoU3!!!
+
+  if (!huskylens.begin(Wire)) {
+    Serial.println(F("HuskyLens begin failed!"));
+  }
+
+  // Sets the Husky Lens to automatically go to tag detection
+  huskylens.writeAlgorithm(ALGORITHM_TAG_RECOGNITION);
+}
+
+
+void updateVision() {
+  if (huskylens.requestBlocks() && huskylens.countBlocks() >= 1){
+    isTagVisable = true;
+
+    // Adjusts the tag data matrix to match sensor readings
+    for (int i = 0; i <= 2; i++) {
+      HUSKYLENSResult result = huskylens.getBlock(i); // get tag with ID zero
+      availableTags[i][1] = i;
+      availableTags[i][1] = result.xCenter;
+      availableTags[i][2] = result.yCenter;
+      availableTags[i][3] = result.width;
+      availableTags[i][4] = result.height;
+    }
+  } else {
+    isTagVisable = false;
+  }
+}
+
+void printVision() {
+  Serial.print(F("tagID= "));  Serial.print(availableTags[0][0]);
+  Serial.print(F(" ,xCenter= ")); Serial.print(availableTags[0][1]);
+  Serial.print(F(" ,yCenter= ")); Serial.print(availableTags[0][2]);
+  Serial.print(F(" ,width= "));   Serial.print(availableTags[0][3]);
+  Serial.print(F(" ,height= "));  Serial.println(availableTags[0][4]); // println here to end the line
+}
