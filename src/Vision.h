e6f0b194c40707c68d8722de87f4e76fc3ac7bcf
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-06 21:11:24 -0400

    Define vision constants in Vision.h
    Added vision constants for tag width, distance, and pixel width.

--- src/Vision.h
+++ src/Vision.h
@@ -2,6 +2,12 @@
 #ifndef VISION_H
 #define VISION_H
 
+// Vision Constants
+const float M_TAG_WIDTH = 16.5;
+const float M_TAG_DISTANCE = 45.7;
+const float M_TAG_PIXEL_WIDTH = 122;
+const float FLOAT_CONSTANT = (M_TAG_PIXEL_WIDTH * M_TAG_DISTANCE) / M_TAG_WIDTH;
+
 // Data Request Functions
 int* get_TagRow(int rowIndex);
 int get_isTagVisable();
