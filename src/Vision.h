3c722474b2218bf42bc73805f1afcacfec01edd3
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-08 17:14:25 -0400

    Change M_TAG_PIXEL_WIDTH to 108
    Updated M_TAG_PIXEL_WIDTH to 108 with alternative values noted.

--- src/Vision.h
+++ src/Vision.h
@@ -5,7 +5,7 @@
 // Vision Constants
 const float M_TAG_WIDTH = 16.5;
 const float M_TAG_DISTANCE = 45.7;
-const float M_TAG_PIXEL_WIDTH = 122;
+const float M_TAG_PIXEL_WIDTH = 108; // Alternative values that work: 118, 122
 const float FOCAL_CONSTANT = (M_TAG_PIXEL_WIDTH * M_TAG_DISTANCE) / M_TAG_WIDTH;
 
 // Data Request Functions
