515bec1549fb7a26fbf005acc9f83df6c36e47cc
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-08 21:49:08 -0400

    Add FIELD_APRIL_TAG_WIDTH constant to Vision.h

--- src/Vision.h
+++ src/Vision.h
@@ -7,6 +7,7 @@ const float M_TAG_WIDTH = 16.5;
 const float M_TAG_DISTANCE = 45.7;
 const float M_TAG_PIXEL_WIDTH = 108; // Alternative values that work: 118, 122
 const float FOCAL_CONSTANT = (M_TAG_PIXEL_WIDTH * M_TAG_DISTANCE) / M_TAG_WIDTH;
+const float FIELD_APRIL_TAG_WIDTH = 16.5; // Changeable based on measured size!
 
 // Data Request Functions
 float* get_TagRow(int rowIndex);
