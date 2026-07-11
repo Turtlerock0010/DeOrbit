3e89b0d8746cd176025097c0ed39085ebc79aa93
Turtlerock0010 <danielrprincipe@gmail.com>
2026-07-10 21:48:21 -0400

    Update robot state management and motor configurations
    Refactor robot control logic and motor settings.

--- src/main.ino
+++ src/main.ino
@@ -33,7 +33,10 @@ NoU_Servo turretServo(turretServoHeader);
 float measured_angle = 27.451;
 float angular_scale = (5.0*2.0*PI) / measured_angle;
 float movementSpeed = 1;
-float turret_servo_angle;
+float turret_servo_angle = 0;
+float* hub_tag_data;
+
+RobotState robot_state = STANDBY;
 
 
 // Functions
@@ -51,19 +54,21 @@ void setup() {
   beginDrivetrain(); // Starts the drivetrain
   beginVision(); // Starts the vision system
 
-  kickerMotor.setInverted(true);
-  shooterMotor.setInverted(false);
-  intakeMotor.setInverted(false);
+  kickerMotor.setInverted(false);
+  shooterMotor.setInverted(true);
+  intakeMotor.setInverted(true);
   spindexerMotor.setInverted(true);
 }
 
 void loop() {
   // Prints robot angle to serial
-  static unsigned long lastPrintTime = 0;
-  if (lastPrintTime + 100 < millis()){
-    Serial.printf("gyro yaw (radians): %.3f\r\n",  NoU3.yaw * angular_scale );
-    lastPrintTime = millis();
-  }
+  //static unsigned long lastPrintTime = 0;
+  //if (lastPrintTime + 100 < millis()){
+  //  Serial.printf("gyro yaw (radians): %.3f\r\n",  NoU3.yaw * angular_scale );
+  //  lastPrintTime = millis();
+  //}
+
+  hub_tag_data = get_TagRow(0);
 
   // This measures your batteries voltage and sends it to PestoLink
   float batteryVoltage = NoU3.getBatteryVoltage();
@@ -72,51 +77,99 @@ void loop() {
   if (PestoLink.isConnected()) {
     // --- Robot Functions ---
 
-    // Spindexer Motor Buttons
-    if (PestoLink.buttonHeld(CIRCLE_BUTTON)) {
-      spindexerMotor.set(1);
-    } else {
-      spindexerMotor.set(0);
+    // Get robot heading (in radians) from the gyro
+    float heading = NoU3.roll * ANGULAR_SCALE;
+
+    static unsigned long lastPrintTime = 0;
+    if (lastPrintTime + 100 < millis()){
+      Serial.printf("heading: %.3f\r\n", heading*180/PI);
+      lastPrintTime = millis();
     }
 
-    // Shooter Motor Buttons
-    if (PestoLink.buttonHeld(RIGHT_TRIGGER_BUTTON)) {
-      shooterMotor.set(1);
+    // State Machine Control Logic
+    if (PestoLink.buttonHeld(RIGHT_TRIGGER_BUTTON) || PestoLink.keyHeld(Key::E)) {
+      // SHOOT
+      robot_state = SHOOT;
+    } else if (PestoLink.buttonHeld(LEFT_TRIGGER_BUTTON) || PestoLink.keyHeld(Key::Q)) {
+      // INTAKE
+      robot_state = INTAKE;
+    } else if (PestoLink.buttonHeld(CIRCLE_BUTTON) || PestoLink.keyHeld(Key::R)) {
+      // STIR
+      robot_state = STIR;
     } else {
-      shooterMotor.set(0);
+      // STANDBY
+      robot_state = STANDBY;
     }
 
-    // Intake Motor Buttons
-    if (PestoLink.buttonHeld(LEFT_TRIGGER_BUTTON)) {
-      intakeMotor.set(1);
-    } else {
-      intakeMotor.set(0);
-    }
 
-    // Kicker Motor Buttons
-    if (PestoLink.buttonHeld(TRIANGLE_BUTTON)) {
-      kickerMotor.set(1);
-    } else {
-      kickerMotor.set(0);
+    // State Machine Execution Logic
+    switch (robot_state) {
+      case SHOOT:
+        shooterMotor.set(1);
+        spindexerMotor.set(1);
+        kickerMotor.set(1);
+        intakeMotor.set(1);
+        break;
+
+      case INTAKE:
+        shooterMotor.set(0);
+        spindexerMotor.set(0);
+        kickerMotor.set(0);
+        intakeMotor.set(1);
+        break;
+
+      case STIR:
+        shooterMotor.set(0);
+        spindexerMotor.set(1);
+        kickerMotor.set(0);
+        intakeMotor.set(1);
+        break;
+
+      case STANDBY:
+      default:
+        shooterMotor.set(0);
+        spindexerMotor.set(0);
+        kickerMotor.set(0);
+        intakeMotor.set(0);
+        break;
     }
+    
 
     // Turret Rotation Buttons
-    if (PestoLink.buttonHeld(LEFT_BUMPER_BUTTON)) {
-      turret_servo_angle++;
+
+    // Set Zero
+    if (PestoLink.keyHeld(Key::T)) {
+      turret_servo_angle = 0;
       turretServo.write(turret_servo_angle);
     }
 
-    if (PestoLink.buttonHeld(RIGHT_BUMPER_BUTTON)) {
-      turret_servo_angle--;
+    if (PestoLink.buttonHeld(SQUARE_BUTTON) || PestoLink.keyHeld(Key::G)) {
+      
+      robot_state = SHOOT;
+      turret_servo_angle = 117;
+      turretServo.write(turret_servo_angle);
+    }
+
+    // Turn Clockwise
+    if (PestoLink.buttonHeld(LEFT_BUMPER_BUTTON) || PestoLink.keyHeld(Key::F)) {
+      if (turret_servo_angle < 220) {
+        turret_servo_angle += 1;
+      }
+      turretServo.write(turret_servo_angle);
+    }
+
+    // Turn Counterclockwise
+    if (PestoLink.buttonHeld(RIGHT_BUMPER_BUTTON) || PestoLink.keyHeld(Key::C)) {
+      turret_servo_angle -= 1;
       turretServo.write(turret_servo_angle);
     }
 
     // Per Dependency Updates
     updateVision(); 
-    updateDrivetrain(PestoLink.getAxis(0), PestoLink.getAxis(1), PestoLink.getAxis(2), movementSpeed);
+    updateDrivetrain(PestoLink.getAxis(0), PestoLink.getAxis(1), movementSpeed);
     NoU3.setServiceLight(LIGHT_ENABLED);
   } else {
-    updateDrivetrain(0, 0, 0, 0);
+    updateDrivetrain(0, 0, 0);
     NoU3.setServiceLight(LIGHT_DISABLED);
   }
 }
