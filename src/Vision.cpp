617f427a3b2962bf52cc415fe17e3503eb638468
Daniel Principe <danielrprincipe@gmail.com>
2026-09-24 13:10:09 -0400

    Added safe implementation of vision
    Updated distance sensor timeout and added initialization check.

--- src/Vision.cpp
+++ src/Vision.cpp
@@ -2,19 +2,24 @@
 
 
 void beginVision() {
-  distanceSensor.setTimeout(100);
-  distanceSensor.init();
-  // Lower the return signal rate limit (default is 0.25) to catch weak, distant reflections
-  //distanceSensor.setSignalRateLimit(0.1);
-  // Increase laser pulse periods (defaults are 14 and 10 PCLKs)
-  //distanceSensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-  //distanceSensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
-  // Reduce timing budget to 20 ms (default is ~33 ms).
-  //distanceSensor.setMeasurementTimingBudget(20000);
-  // Tells the sensor to continuously stream readings back-to-back at hardware speed
+  distanceSensor.setTimeout(500);
+  
+  // Checks if distance sensor initializes
+  if (!distanceSensor.init()) {
+    Serial.println("VL53L0X initialization failed");
+    return;
+  }
+
   distanceSensor.startContinuous();
 }
 
 float readDistanceCm() {
-  return distanceSensor.readRangeContinuousMillimeters() / 10.0;
+  uint16_t millimeters = distanceSensor.readRangeContinuousMillimeters();
+
+  if (distanceSensor.timeoutOccurred()) {
+    Serial.println("VL53L0X timeout");
+    return NAN;
+  }
+
+  return millimeters / 10.0f;
 }
